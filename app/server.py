f732d176295fefe2a53d106154c6d7a0d69c221b
b.mayr <b.mayr@we-make.ai>
2020-02-06 16:04:17 +0100

    ...

---
+++ app/server.py
@@ -0,0 +1,70 @@
+import aiohttp
+import asyncio
+import uvicorn
+from fastai2.vision.all import *
+from io import BytesIO
+from starlette.applications import Starlette
+from starlette.middleware.cors import CORSMiddleware
+from starlette.responses import HTMLResponse, JSONResponse
+from starlette.staticfiles import StaticFiles
+
+# Liegt auf gdrive von elexis.austria@gmail.com
+export_file_url = 'https://drive.google.com/file/d/1abQnRRF0s8C3AVMK3EMhVGk2dzHLfy5T/view?usp=sharing'
+export_file_name = 'export.pkl'
+
+classes = ['Saab_9000', 'Saab_900', 'Saab_9-3', 'Saab_9-5']
+path = Path(__file__).parent
+
+app = Starlette()
+app.add_middleware(CORSMiddleware, allow_origins=['*'], allow_headers=['X-Requested-With', 'Content-Type'])
+app.mount('/static', StaticFiles(directory='app/static'))
+
+
+async def download_file(url, dest):
+    if dest.exists(): return
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            data = await response.read()
+            with open(dest, 'wb') as f:
+                f.write(data)
+
+
+async def setup_learner():
+    await download_file(export_file_url, path / export_file_name)
+    try:
+        learn = torch.load(path/export_file_name)
+        return learn
+    except RuntimeError as e:
+        if len(e.args) > 0 and 'CPU-only machine' in e.args[0]:
+            print(e)
+            message = "\n\nThis model was trained with an old version of fastai and will not work in a CPU environment.\n\nPlease update the fastai library in your training environment and export your model again.\n\nSee instructions for 'Returning to work' at https://course.fast.ai."
+            raise RuntimeError(message)
+        else:
+            raise
+
+
+loop = asyncio.get_event_loop()
+tasks = [asyncio.ensure_future(setup_learner())]
+learn = loop.run_until_complete(asyncio.gather(*tasks))[0]
+loop.close()
+
+
+@app.route('/')
+async def homepage(request):
+    html_file = path / 'view' / 'index.html'
+    return HTMLResponse(html_file.open().read())
+
+
+@app.route('/analyze', methods=['POST'])
+async def analyze(request):
+  img_data = await request.form()
+  img_bytes = await (img_data['file'].read())
+  pred = learn.predict(BytesIO(img_bytes))[0]
+  return JSONResponse({
+      'results': str(pred)
+  })
+
+
+if __name__ == '__main__':
+    if 'serve' in sys.argv:
+        uvicorn.run(app=app, host='0.0.0.0', port=5000, log_level="info")
